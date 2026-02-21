99c5f1cc9eb32ab6a6783edd3d7e3c257f094530
liaqat-paindah <liaqat.paindah@email.com>
2026-02-22 02:57:26 +0430

    feat: add Services component to Home page and adjust padding in Services section

--- components/home/page.tsx
+++ components/home/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import AyandahaDigitalCarousel from "./carousel";
 import NexusScholarships from "../scholarships/page";
+import Services from "../services/page";
 
 const Home = () => {
   return (
     <div>
       <AyandahaDigitalCarousel></AyandahaDigitalCarousel>
       <NexusScholarships></NexusScholarships>
+      <Services></Services>
     </div>
   );
 };
